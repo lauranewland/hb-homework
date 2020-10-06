3178853ed76f8033b02811089cf811b23132bb35
Laura Newland <laurasmora101@gmail.com>
2020-10-06 03:38:45 +0000

    Finial Commit w/ Comments

--- accounting-scripts/melon_info.py
+++ accounting-scripts/melon_info.py
@@ -1,17 +1,14 @@
 """Print out all the melons in our inventory."""
 
-
 from melons import melon
 
 
 def print_melon(melon):
     """Print each melon with corresponding attribute information."""
     
-    for key, value in melon.items():
+    for key, values in melon.items(): #loops through the Key(Melon Names) with the values listed in a dictionary
         print(key)
-        for attribute, value2 in value.items():
-            print(f"     {attribute} : {value2}")
+        for attribute, value in values.items(): #loops each attribute(price, weight, flesh color, etc...)
+            print(f"     {attribute} : {value}")
             
-
-
 print_melon(melon)
