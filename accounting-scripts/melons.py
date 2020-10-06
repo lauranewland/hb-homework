54696db0052010c3d65581bf6dfb46632e9f7aa1
Laura Newland <laurasmora101@gmail.com>
2020-10-06 03:39:43 +0000

    Added Melon Location Attribute

--- accounting-scripts/melons.py
+++ accounting-scripts/melons.py
@@ -4,61 +4,66 @@ melon = {
         "seedlessness" : True,
         "flesh_color" : "Green",
         "rind_color" : "Lime",
-        "average_weight" : 1.00
+        "average_weight" : 1.00,
+        "grown_location" : None
     },
      'Crenshaw' : {
         "price" : 2.00,
         "seedlessness" : False,
         "flesh_color" : "Yellow Green",
         "rind_color" : "Yellow",
-        "average_weight" : 1.25
+        "average_weight" : 1.25,
+        "grown_location" : None
      },
     'Crane' : {
         "price" : 2.50,
         "seedlessness" : False,
         "flesh_color" : "Orange",
         "rind_color" : "Orange",
-        "average_weight" : 1.50
+        "average_weight" : 1.50,
+        "grown_location" : None
     },
     'Casaba' : {
         "price" : 2.50,
         "seedlessness" : False,
         "flesh_color" : "Yellow",
         "rind_color" : "White",
-        "average_weight" : 1.25
+        "average_weight" : 1.25,
+        "grown_location" : None
     },
     'Cantaloupe' : {
         "price" : 0.99,
         "seedlessness" : False,
         "flesh_color" : "Tan",
         "rind_color" : "Orange",
-        "average_weight" : 1.75   
+        "average_weight" : 1.75, 
+        "grown_location" : None
     }
 }
 
-melon_names = {
-    1: 'Honeydew',
-    2: 'Crenshaw',
-    3: 'Crane',
-    4: 'Casaba',
-    5: 'Cantaloupe',
-}
+# melon_names = {
+#     1: 'Honeydew',
+#     2: 'Crenshaw',
+#     3: 'Crane',
+#     4: 'Casaba',
+#     5: 'Cantaloupe',
+# }
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
-}
+# melon_prices = {
+#     1: 0.99,
+#     2: 2.00,
+#     3: 2.50,
+#     4: 2.50,
+#     5: 0.99,
+# }
 
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
-}
+# melon_seedlessness = {
+#     1: True,
+#     2: False,
+#     3: False,
+#     4: False,
+#     5: False,
+# }
 
 
 
