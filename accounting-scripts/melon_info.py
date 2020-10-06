4abba7d129e4013844a8012b8f22552a60fed42d
Laura Newland <laurasmora101@gmail.com>
2020-10-06 03:30:28 +0000

    Created For Loop to Print all new Melon info

--- accounting-scripts/melon_info.py
+++ accounting-scripts/melon_info.py
@@ -1,18 +1,17 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+from melons import melon
 
 
-def print_melon(name, seedless, price):
+def print_melon(melon):
     """Print each melon with corresponding attribute information."""
+    
+    for key, value in melon.items():
+        print(key)
+        for value1, value2 in value.items():
+            print(f"     {value1} : {value2}")
+            
 
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
 
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
-
-
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i], melon_flesh_color[i], melon_rind_color[i], average_melon_weight[i])
+print_melon(melon)
